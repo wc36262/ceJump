62d6f9ca05ff001b1fc2aef6c23183ba281723b3
peterwang <peterwang36262@gmail.com>
2020-07-08 22:36:30 +0800

    object,场景，跳跃的棋子

--- src/block/base.js
+++ src/block/base.js
@@ -1,8 +1,22 @@
+
+import blockConf from '../../confs/block-conf'
+import { customAnimation } from '../../libs/animation'
+
 export default class BaseBlock {
-  constructor(type) {
-    this.type = type
-    
+  constructor (type) {
+    this.type = type // cuboid | cylinder
+    this.height = blockConf.height
+    this.width = blockConf.width
+    this.status = 'stop'
+    this.scale = 1
   }
+
+
+  popup () {
+    this.instance.position.set(this.x, this.y + 30, this.z)
+    customAnimation.to(this.instance.position, 0.5, { x: this.x, y: this.y, z: this.z, ease: 'Bounce.easeOut' })
+  }
+
 
 
 }
